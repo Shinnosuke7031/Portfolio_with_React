cb72b2ec33b2eba1066cc8e22126c4138b28c739
飯野信之介 <iino@iinoMacBookPro.local>
2020-08-03 12:55:36 +0900

    Add Othello to Portfolio

---
+++ docs/precache-manifest.9850d91636d9cd03e521f231a3c5412d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df00af0be9b6f6bb50ade97f64c460cd",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "7d6566957c89d4002910",
+    "url": "/react-test/static/css/2.b549c411.chunk.css"
+  },
+  {
+    "revision": "7d6566957c89d4002910",
+    "url": "/react-test/static/js/2.51a2a91e.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/react-test/static/js/2.51a2a91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc42dca6262a67dc399",
+    "url": "/react-test/static/js/main.93a82719.chunk.js"
+  },
+  {
+    "revision": "14d041c37aa1e787e0b2",
+    "url": "/react-test/static/js/runtime-main.08fd1e44.js"
+  },
+  {
+    "revision": "81da1d862be2131a4b839ad69814166a",
+    "url": "/react-test/static/media/chatAPP1.81da1d86.jpg"
+  },
+  {
+    "revision": "83c5e3c7964f48358108f9a32787249b",
+    "url": "/react-test/static/media/chatAPP2.83c5e3c7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-test/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-test/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-test/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-test/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-test/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "f6367b18b6ecbaedcb02ba9eba3d6dc8",
+    "url": "/react-test/static/media/nosukeMemo1.f6367b18.jpg"
+  },
+  {
+    "revision": "7df082804eee1a99bbd49eb63dfcb33a",
+    "url": "/react-test/static/media/nosukeMemo2.7df08280.jpg"
+  },
+  {
+    "revision": "7dbae4e3c78e57b26a3ebfb52f4ba434",
+    "url": "/react-test/static/media/othello1.7dbae4e3.png"
+  },
+  {
+    "revision": "9373dbfaf44ba54b22eeab603eda3f65",
+    "url": "/react-test/static/media/othello2.9373dbfa.png"
+  }
+]);
