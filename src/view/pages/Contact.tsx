7502163810c63ea31236ab1460389ae6789fd5f2
Shinnosuke Iino <sengoku731sin@gmail.com>
2020-10-06 17:13:18 +0900

    Update Contact CSS

--- src/view/pages/Contact.tsx
+++ src/view/pages/Contact.tsx
@@ -9,24 +9,24 @@ const Contact=()=>{
   return(
     <div>
       <h1 className={styles.title}>Contact</h1>
-        <div>
+        <div className={styles.contact}>
           <MediaQuery query="(min-width: 769px)">
-            <div>
-              <FontAwesomeIcon icon={faEnvelope} />
-              <p>8120505@ed.tus.ac.jp</p>             
+            <div className={styles.mail}>
+              <FontAwesomeIcon icon={faEnvelope}/>
+              <a className={`${styles.show_line_from_left} ${styles.change_color_from_left}`} href="mailto:8120505@ed.tus.ac.jp">8120505@ed.tus.ac.jp</a>
             </div>
           </MediaQuery>
           <MediaQuery query="(max-width: 768px)">
             <div>
               <FontAwesomeIcon icon={faEnvelope} />
-              <p>8120505@ed.tus.ac.jp</p>             
+              <a href="mailto:8120505@ed.tus.ac.jp">8120505@ed.tus.ac.jp</a>             
             </div>
           </MediaQuery>
             
             <hr/>
             <div>
-              <a href="https://github.com/Shinnosuke7031"><i className="fa fa-github fa-2x" style={{color: "black"}}></i> </a>
-              <a href="https://twitter.com/web7031boushi"><i className="fa fa-twitter fa-2x" style={{color: "black"}}></i> </a>
+              <a href="https://github.com/Shinnosuke7031"><i className={`fa fa-github fa-2x ${styles.contact_icon}`}></i> </a>
+              <a href="https://twitter.com/web7031boushi"><i className={`fa fa-twitter fa-2x ${styles.contact_icon}`}></i> </a>
             </div>
         </div>
     </div>    
