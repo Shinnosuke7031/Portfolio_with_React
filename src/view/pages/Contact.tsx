dd0a8cb347829cb8346c2018ff050b24ebc50e4c
Shinnosuke Iino <sengoku731sin@gmail.com>
2020-10-10 19:26:02 +0900

    Divide Contact.tsx into breakpoints

--- src/view/pages/Contact.tsx
+++ src/view/pages/Contact.tsx
@@ -1,35 +1,27 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";//これでもFontAwesome使える
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import MediaQuery from "react-responsive";
 import styles from './pages.module.css';
+import ContactInfoes from '../templates/PC_Components/ContactInfoes';
+import ContactInfoesMob from '../templates/MobileComponents/ContactInfoesMob';
 
-const Contact=()=>{
+const Contact: FC<{}> = () => {
   
   return(
     <div>
       <h1 className={styles.title}>Contact</h1>
-        <div className={styles.contact}>
-          <MediaQuery query="(min-width: 769px)">
-            <div className={styles.mail}>
-              <FontAwesomeIcon icon={faEnvelope}/>
-              <a className={`${styles.show_line_from_left} ${styles.change_color_from_left}`} href="mailto:8120505@ed.tus.ac.jp">8120505@ed.tus.ac.jp</a>
-            </div>
-          </MediaQuery>
-          <MediaQuery query="(max-width: 768px)">
-            <div>
-              <FontAwesomeIcon icon={faEnvelope} />
-              <a href="mailto:8120505@ed.tus.ac.jp">8120505@ed.tus.ac.jp</a>             
-            </div>
-          </MediaQuery>
-            
-            <hr/>
-            <div>
-              <a href="https://github.com/Shinnosuke7031"><i className={`fa fa-github fa-2x ${styles.contact_icon}`}></i> </a>
-              <a href="https://twitter.com/web7031boushi"><i className={`fa fa-twitter fa-2x ${styles.contact_icon}`}></i> </a>
-            </div>
-        </div>
-    </div>    
+      
+        <MediaQuery query="(min-width: 769px)">
+          <ContactInfoes />
+        </MediaQuery>
+
+        <MediaQuery query="(max-width: 768px)">
+          <ContactInfoesMob />
+        </MediaQuery>
+
+      </div>
+    
   );
 }
 export default Contact;
---
+++ src/view/templates/MobileComponents/ContactInfoesMob.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import styles from './mob.module.css';
+
+const ContactInfoesMob: FC<{}> = () => {
+
+  return (
+    <div className={styles.contact}>
+      <div className={styles.mail}>
+        <FontAwesomeIcon icon={faEnvelope}/>
+        <a className={`${styles.show_line_from_left} ${styles.change_color_from_left}`} href="mailto:8120505@ed.tus.ac.jp">8120505@ed.tus.ac.jp</a>
+      </div>
+      <hr/>
+      <div>
+        <a href="https://github.com/Shinnosuke7031"><i className={`fa fa-github fa-2x ${styles.contact_icon}`}></i> </a>
+        <a href="https://twitter.com/web7031boushi"><i className={`fa fa-twitter fa-2x ${styles.contact_icon}`}></i> </a>
+      </div>
+    </div>
+  );
+}
+
+export default ContactInfoesMob;
