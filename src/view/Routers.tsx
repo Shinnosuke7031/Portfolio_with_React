20e35c9dcbaf4bbfe05504fa78312c5fae858c27
Shinnosuke Iino <sengoku731sin@gmail.com>
2021-05-20 21:20:47 +0900

    add react-helmet

--- src/view/Routers.tsx
+++ src/view/Routers.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import {Helmet} from "react-helmet";
 import Header from './organisms/Header';
 import Top from './pages/Top';
 import Aboutme from './pages/Aboutme';
@@ -10,20 +11,25 @@ import styles from './routers.module.css';
 
 const Routers: FC<{}> = () => {
   return (
-    <Router>
-      <div className={styles.container}>
-        <Header />
-        <div className={styles.wrapper}>
-          <Switch>
-            <Route exact path={"/"} component={Top} />
-            <Route exact path={"/aboutme"} component={Aboutme} />
-            <Route exact path={"/output"} component={Portfolio} />
-            <Route exact path={"/contact"} component={Contact} />
-            <Route exact path={"/about-this-site"} component={AboutThisSite} />
-          </Switch>
+    <React.Fragment>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+      <Router>
+        <div className={styles.container}>
+          <Header />
+          <div className={styles.wrapper}>
+            <Switch>
+              <Route exact path={"/"} component={Top} />
+              <Route exact path={"/aboutme"} component={Aboutme} />
+              <Route exact path={"/output"} component={Portfolio} />
+              <Route exact path={"/contact"} component={Contact} />
+              <Route exact path={"/about-this-site"} component={AboutThisSite} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </React.Fragment>
   );
 };
 
